48108535e166f9a9f381647e3ffd71ed73abf81c
Ostap Stefanyshyn <ostefanyshyn@icloud.com>
2024-05-11 12:28:20 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Введіть натуральне число: ");
+        double n = Math.floor(myObj.nextDouble());
+        double S = 1 - 2/3 +3/7 - 4/9 +;
+        System.out.println("S =  " + S);
+
+    }
+}
