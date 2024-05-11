69fd90ac58884515dff7f090cd300a0920f0fedd
ostefanyshyn <147067379+ostefanyshyn@users.noreply.github.com>
2024-05-11 12:57:43 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,16 @@ public class Main
         Scanner myObj = new Scanner(System.in);
         System.out.println("Введіть натуральне число: ");
         double n = Math.floor(myObj.nextDouble());
-        double S = 1 - 2/3 +3/7 - 4/9 +;
+        double S = 0;
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                S += 1/Math.pow(2, i);
+            }
+            if(i % 2 == 1){
+                S -= 1/Math.pow(2, i);
+            }
+        }
         System.out.println("S =  " + S);
-
     }
 }
+
